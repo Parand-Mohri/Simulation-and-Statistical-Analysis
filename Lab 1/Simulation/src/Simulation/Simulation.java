4dfabbed0c791a93379d35c33205fe68be684fca
Parand <p.mohri@student.maastrichtuniversity.nl>
2022-04-08 12:41:40 +0200

    done

--- Lab 1/Simulation/src/Simulation/Simulation.java
+++ Lab 1/Simulation/src/Simulation/Simulation.java
@@ -23,14 +23,18 @@ public class Simulation {
 	CEventList l = new CEventList();
 	// A queue for the machine
 	Queue q = new Queue();
+
+    double[] ai = {0.4, 1.2 , 0.5 ,1.7, 0.2 , 1.6 ,0.2 ,1.4, 1.9};
 	// A source
-	Source s = new Source(q,l,"Source 1");
+	Source s = new Source(q,l,"Source 1", ai);
 	// A sink
 	Sink si = new Sink("Sink 1");
+
+	double[] st = {2.0, 0.7, 0.2, 1.1,3.7,0.6,4.0,4.0,4.0};
 	// A machine
-	Machine m = new Machine(q,si,l,"Machine 1");
+	Machine m = new Machine(q,si,l,"Machine 1", st);
 	// start the eventlist
-	l.start(2000); // 2000 is maximum time
+	l.start(8.7); // 2000 is maximum time
     }
     
 }
